2a806d018c05fa68c8e6620a50a11afaec89a99d
NASDAMI Quatadah <quatadah.nasdami@gmail.com>
2023-01-16 18:31:16 +0100

    Front end initialized

---
+++ front/src/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/system";
+import React from "react";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+    return (
+        <Box>
+            <h1>Dashboard</h1>
+        </Box>
+    );
+};
+
+export default Dashboard;
